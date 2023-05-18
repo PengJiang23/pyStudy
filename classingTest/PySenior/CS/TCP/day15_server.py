88002264d8f02935d2a1c3cdfff7003c9d3f06ea
tree951 <j.tree.fred951@gmail.com>
2023-05-18 16:21:14 +0800

    Initial commit

--- classingTest/PySenior/CS/TCP/day15_server.py
+++ classingTest/PySenior/CS/TCP/day15_server.py
@@ -1,6 +1,16 @@
 import socket
 
 def tcp_server():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     addr = ('192.168.31.218',2000)
     s.bind(addr)
+    s.listen(128) # 监听数目
+    client_socket, client_addr = s.accept()
+    data = client_socket.recv(100)
+    print(data.decode('utf8'))
+    client_socket.send('来自服务器的数据'.encode('utf8'))
+    client_socket.close()
+    s.close()
+
+if __name__ == '__main__':
+    tcp_server()
